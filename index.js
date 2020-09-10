07a78c4bec1b97e4e1adfbe404d8c2dc3f55e7e0
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-09-10 12:48:42 +0530

    main file

---
+++ index.js
@@ -0,0 +1,29 @@
+const fastify = require('fastify')()
+const fetch = require('node-fetch')
+
+fastify.register(require('fastify-url-data'))
+
+fastify.get('/*', async (req, reply) => {
+	const { path } = requrlData()
+	const url = 'http://' + decodeURIComponent(path).substring(1)
+		.then(res => res.text())
+		.catch(err => {
+			if (err) {
+				reply
+					.code(400)
+					.send({
+						statusCode: 400,
+						message: 'BAD REQUEST: ' + err.message
+					})
+			}
+		})
+
+	const matches = rawText.match(/<title>(.+)<\/title>/)
+	const title = matches ? matches[1] : ''
+
+	reply.send(title)
+})
+
+fastify.listen(process.env.PORT || 3000, err => {
+	if (err) throw err
+})
