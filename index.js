086ce08901ed7c353ecf07298bbd9ec6e78b5d91
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-09-10 12:52:31 +0530

    revised & refactored v1.0.1

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch')
 fastify.register(require('fastify-url-data'))
 
 fastify.get('/*', async (req, reply) => {
-	const { path } = requrlData()
+	const { path } = req.urlData()
 	const url = 'http://' + decodeURIComponent(path).substring(1)
+	const rawText = await fetch(url)
 		.then(res => res.text())
 		.catch(err => {
 			if (err) {
